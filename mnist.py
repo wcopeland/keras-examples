3137a6ce3c84338cc545f24ae365f1a7b1c442bd
Adrian Yi <sjay87@gmail.com>
2018-09-06 13:05:37 -0700

    Implemented iterator initializer hook

--- mnist.py
+++ mnist.py
@@ -6,25 +6,28 @@ import numpy as np
 import tensorflow as tf
 from clusterone import get_data_path, get_logs_path
 
-from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
+from tensorflow.examples.tutorials.mnist.input_data import read_data_sets
 
 tf.logging.set_verbosity(tf.logging.INFO)
 try:
-    config = os.environ['TF_CONFIG']
-    config = json.loads(config)
-    task = config['task']['type']
-    task_index = config['task']['index']
-
-    local_ip = 'localhost:' + config['cluster'][task][task_index].split(':')[1]
-    config['cluster'][task][task_index] = local_ip
-    if task == 'chief' or task == 'master':
-        config['cluster']['worker'][task_index] = local_ip
-    os.environ['TF_CONFIG'] = json.dumps(config)
-except:
-    job_name = None
-    task_index = 0
-    ps_hosts = None
-    worker_hosts = None
+    job_name = os.environ['JOB_NAME']
+    task_index = int(os.environ['TASK_INDEX'])
+    ps_hosts = os.environ['PS_HOSTS'].split(',')
+    worker_hosts = os.environ['WORKER_HOSTS'].split(',')
+    TF_CONFIG = {'task': {'type': job_name, 'index': task_index},
+                 'cluster': {'chief': [worker_hosts[0]],
+                             'worker': worker_hosts,
+                             'ps': ps_hosts},
+                 'environment': 'cloud'}
+    local_ip = 'localhost:' + TF_CONFIG['cluster'][job_name][task_index].split(':')[1]
+    if (job_name == 'chief') or (job_name == 'worker' and task_index == 0):
+        job_name = 'chief'
+        TF_CONFIG['task']['type'] = 'chief'
+        TF_CONFIG['cluster']['worker'][0] = local_ip
+    TF_CONFIG['cluster'][job_name][task_index] = local_ip
+    os.environ['TF_CONFIG'] = json.dumps(TF_CONFIG)
+except KeyError as ex:
+    pass
 
 def get_args():
     '''Return parsed args'''
@@ -65,6 +68,47 @@ def get_model(opts):
     print(model.summary())
     return model
 
+class IteratorInitializerHook(tf.train.SessionRunHook):
+    '''From https://medium.com/onfido-tech/higher-level-apis-in-tensorflow-67bfb602e6c0'''
+    def __init__(self):
+        super(IteratorInitializerHook, self).__init__()
+        self.iterator_initializer_func = None
+
+    def after_create_session(self, session, coord):
+        self.iterator_initializer_func(session)
+
+def get_inputs(x, y, batch_size=64, train=True):
+    '''Returns input function and and iterator initializer hook'''
+    iterator_initializer_hook = IteratorInitializerHook()
+
+    def input_fn():
+        '''Input function to be used for Estimator class'''
+        x_placeholder = tf.placeholder(tf.float32, x.shape)
+        y_placeholder = tf.placeholder(tf.float32, y.shape)
+        data_x = tf.data.Dataset.from_tensor_slices(x_placeholder)
+        data_y = tf.data.Dataset.from_tensor_slices(y_placeholder)
+        data = tf.data.Dataset.zip((data_x, data_y))
+        data = data.batch(batch_size)
+        if train:
+            data = data.repeat(count=None).shuffle(buffer_size=5*batch_size)
+        else:
+            data = data.repeat(count=1)
+
+        data = data.prefetch(8)
+
+        iterator = data.make_initializable_iterator()
+        next_example, next_label = iterator.get_next()
+
+        # Set runhook to initialize iterator
+        iterator_initializer_hook.iterator_initializer_func = lambda sess: sess.run(
+            iterator.initializer,
+            feed_dict={x_placeholder: x, y_placeholder: y}
+        )
+
+        return next_example, next_label
+
+    return input_fn, iterator_initializer_hook
+
 def main(opts):
     '''Main function'''
     data = read_data_sets(opts.data_dir,
@@ -92,10 +136,23 @@ def main(opts):
                          num_epochs=1,
                          shuffle=False)
 
+    train_input_fn, train_iter_hook = get_inputs(data.train.images,
+        tf.keras.utils.to_categorical(data.train.labels.astype(np.int32), 10).astype(np.float32),
+        batch_size=opts.batch_size,
+        train=True)
+    eval_input_fn, eval_iter_hook = get_inputs(data.test.images,
+        tf.keras.utils.to_categorical(data.test.labels.astype(np.int32), 10).astype(np.float32),
+        batch_size=opts.batch_size,
+        train=False)
+
     train_spec = tf.estimator.TrainSpec(input_fn=train_input_fn,
-                                        max_steps=1e6)
+                                        max_steps=1e6,
+                                        hooks=[train_iter_hook])
     eval_spec = tf.estimator.EvalSpec(input_fn=eval_input_fn,
-                                      throttle_secs=5)
+                                      steps=None,
+                                      start_delay_secs=0,
+                                      throttle_secs=60,
+                                      hooks=[eval_iter_hook])
 
     tf.estimator.train_and_evaluate(classifier, train_spec, eval_spec)
 
